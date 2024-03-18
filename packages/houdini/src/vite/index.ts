b9f9945320c0f9a6c7406219ce48313d32aafe7e
Seppe Dekeyser <seppe.dekeyser@hotmail.com>
2024-03-18 22:27:16 +0100

    Add support for component queries

--- packages/houdini/src/vite/index.ts
+++ packages/houdini/src/vite/index.ts
@@ -27,8 +27,8 @@ export default function (opts?: PluginConfig): Plugin[] {
 		watch_local_schema(watchSchemaListref),
 		watchAndRun([
 			{
-				name: 'Houdini',
-				// quiet: true,
+                name: 'Houdini',
+                logs: [],
 				async watchFile(filepath: string) {
 					// load the config file
 					const config = await getConfig(opts)
